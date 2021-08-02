98760f1c49633303b29a3c610babaaeddaa512c1
medleycodacy <48563202+medleycodacy@users.noreply.github.com>
2021-08-02 15:47:51 +0100

    medium

---
+++ medium.sh
@@ -0,0 +1,3 @@
+pvcreate ${DEVICE}
+wget -q https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
+rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
